bf0b45b9431399f404c7e824a6b9594a681b7213
davSec01 <145743628+davSec01@users.noreply.github.com>
2023-11-07 14:37:43 +0700

    Add files via upload

---
+++ davin_rpl/script.js
@@ -0,0 +1,5 @@
+let panjang = parseInt(prompt("Masukkan panjang: "));
+let lebar = parseInt(prompt("Masukkan lebar: "));
+let luas = panjang * lebar;
+
+console.log("Luas persegi panjang: " + luas);
